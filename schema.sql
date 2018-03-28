bf2f08ba09df571ca6b8bfff8f93aa7198352046
Daniel Flores <mr.danielfo@gmail.com>
2018-03-28 06:22:59 -0600

    básico SQL
    archivo con las indicaciones básicas para la ejecución en PHP myAdmin

--- schema.sql
+++ schema.sql
@@ -5,3 +5,28 @@
 CREATE DATABASE IF NOT EXISTS cronograma;
 
 USE cronograma;
+
+/* Tabla catálogo */
+
+CREATE TABLE prioridad(
+  priority_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  priority VARCHAR(55) NOT NULL
+);
+
+
+CREATE TABLE proyectos(
+  project_id CHAR(2) PRIMARY KEY,
+  title VARCHAR(80) NOT NULL,
+  priority INTEGER UNSIGNED NOT NULL,
+  time_dev VARCHAR(55) DEFAULT '2 semanas',
+  category ENUM('Curso', 'Proyecto Propio'),
+  FULLTEXT KEY search(title, time_dev, category),
+  FOREIGN KEY (priority) REFERENCES prioridad(priority_id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+INSERT INTO prioridad (priority_id, priority) VALUES
+(1, 'Terminado'),
+(2, 'Alta'),
+(3, 'Media'),
+(4, 'Baja'),
+(5, 'Sin definir');
