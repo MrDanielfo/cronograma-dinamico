a800ddb6d036faa211eaea39e0b48be8b242fdcf
Daniel Flores <mr.danielfo@gmail.com>
2018-03-28 05:04:23 -0600

    Index y Styles
    Se añade el archivo principal index.php, junto con el de los estilos

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Cronograma de Proyectos Web</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Padauk:400,700" rel="stylesheet">
+</head>
+<body>
+  <header class="header-contenedor">
+      <h1 class="titulo">Cronograma de proyectos web de abril a agosto de 2018</h1>
+  </header>
+  <section class="contenedor-secundario">
+      <h2 class=subtitulo>Tabla con los proyectos a realizar</h2>
+      <table class="tabla">
+        <tr>
+          <th class="cabezal">Nombre del Proyecto</th>
+          <th class="cabezal">Prioridad</th>
+          <th class="cabezal">Tiempo de desarrollo</th>
+        </tr>
+        <tr class="finalizada">
+          <td class="proyecto">Plantilla Cómic Primera Fase</td>
+          <td class="prioridad">Alta</td>
+          <td class="desarrollo">3 semanas</td>
+        </tr>
+        <tr class="alta">
+          <td class="proyecto">Curso Symfony Blog</td>
+          <td class="prioridad">Alta</td>
+          <td class="desarrollo">1 mes y una semana</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Curso POO Mexflix</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">1 mes y medio</td>
+        </tr>
+        <tr class="baja">
+          <td class="proyecto">Curso Angular - Fernando Herrera (Sólo Typescript)</td>
+          <td class="prioridad">Baja</td>
+          <td class="desarrollo">1 semana</td>
+        </tr>
+        <tr class="alta">
+          <td class="proyecto">Curso de Git</td>
+          <td class="prioridad">Alta</td>
+          <td class="desarrollo">3 semanas</td>
+        </tr>
+        <tr class="alta">
+          <td class="proyecto">Plantilla Car Primera Fase</td>
+          <td class="prioridad">Alta</td>
+          <td class="desarrollo">2 semanas</td>
+        </tr>
+        <tr class="no-definida">
+          <td class="proyecto">Proyecto Propio de Symfony</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">No definido aún</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Curso JavaScript Moderno</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">1 mes y medio</td>
+        </tr>
+        <tr class="baja">
+          <td class="proyecto">Plantila Vikingo Primera Fase</td>
+          <td class="prioridad">Baja</td>
+          <td class="desarrollo">3 semanas</td>
+        </tr>
+        <tr class="no-definida">
+          <td class="proyecto">Curso de sql EdTeam</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">No definido aún</td>
+        </tr>
+        <tr class="baja">
+          <td class="proyecto">Curso Laravel - Víctor Robles</td>
+          <td class="prioridad">Baja</td>
+          <td class="desarrollo">1 mes</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Curso Front-end Angular y Back-end Symfony</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">1 mes</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Curso Desarrollo Web Completo - De la Torre</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">2 meses</td>
+        </tr>
+        <tr class="baja">
+          <td class="proyecto">Curso WooComerce - De la Torre</td>
+          <td class="prioridad">Baja</td>
+          <td class="desarrollo">1 mes</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Diseño y Desarrollo de Cuarta Plantilla (Landing-Page)</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">1 semana</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Diseño y Desarrollo de Quinta Plantilla (Landing-Page)</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">1 semana</td>
+        </tr>
+        <tr class="baja">
+          <td class="proyecto">Curso Fullstack Angular y Symfony</td>
+          <td class="prioridad">Baja</td>
+          <td class="desarrollo">2 meses</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Diseño y Desarrollo de Sexta Plantilla</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">2 semanas</td>
+        </tr>
+        <tr class="media">
+          <td class="proyecto">Práctica con API de Inegi</td>
+          <td class="prioridad">Media</td>
+          <td class="desarrollo">Constante</td>
+        </tr>
+        <tr class="no-definida">
+          <td class="proyecto">Desarrollo de API para periodistas</td>
+          <td class="prioridad">Baja</td>
+          <td class="desarrollo">No definido aún</td>
+        </tr>
+      </table>
+  </section>
+</body>
+</html>
